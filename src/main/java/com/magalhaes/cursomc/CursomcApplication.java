6524e21ee05e2df3b74e13b5c54e7e35655a4474
Alisson Magalhaes <alisson08silva@gmail.com>
2022-01-11 20:19:21 -0300

    Instanciando um objeto

--- src/main/java/com/magalhaes/cursomc/CursomcApplication.java
+++ src/main/java/com/magalhaes/cursomc/CursomcApplication.java
@@ -1,13 +1,32 @@
 package com.magalhaes.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.magalhaes.cursomc.domain.Categoria;
+import com.magalhaes.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository RepoCat;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null,"Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		RepoCat.saveAll(Arrays.asList(cat1,cat2));
+		
 	}
 
 }
