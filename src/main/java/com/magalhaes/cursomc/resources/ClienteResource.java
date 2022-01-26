02302e540b28ad06f226a758745748c92cc2b253
Alisson Magalhaes <alisson08silva@gmail.com>
2022-01-25 21:57:06 -0300

    Method Post Client

--- src/main/java/com/magalhaes/cursomc/resources/ClienteResource.java
+++ src/main/java/com/magalhaes/cursomc/resources/ClienteResource.java
@@ -1,7 +1,10 @@
 package com.magalhaes.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -12,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.magalhaes.cursomc.domain.Cliente;
 import com.magalhaes.cursomc.dto.ClienteDTO;
+import com.magalhaes.cursomc.dto.ClienteNewDTO;
 import com.magalhaes.cursomc.services.ClienteService;
 
 //Controladores Rest, a fase inicial de contato usuários ela usa os métodos do services
@@ -65,5 +70,14 @@ public class ClienteResource {
 		Page<ClienteDTO> listDTO = list.map(obj -> new ClienteDTO(obj));
 		return ResponseEntity.ok().body(listDTO);
 	}
+	
+	@RequestMapping(method = RequestMethod.POST) // Método post simples no REST
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO objDto) {
+		Cliente obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
 
 }
--- src/main/java/com/magalhaes/cursomc/services/ClienteService.java
+++ src/main/java/com/magalhaes/cursomc/services/ClienteService.java
@@ -9,10 +9,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.magalhaes.cursomc.domain.Cidade;
 import com.magalhaes.cursomc.domain.Cliente;
+import com.magalhaes.cursomc.domain.Endereco;
+import com.magalhaes.cursomc.domain.enums.TipoCliente;
 import com.magalhaes.cursomc.dto.ClienteDTO;
+import com.magalhaes.cursomc.dto.ClienteNewDTO;
 import com.magalhaes.cursomc.repositories.ClienteRepository;
+import com.magalhaes.cursomc.repositories.EnderecoRepository;
 import com.magalhaes.cursomc.services.exceptions.DateIntegrityViolationException;
 import com.magalhaes.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -22,6 +28,9 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+	
+	@Autowired
+	private EnderecoRepository endRepo;
 
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
@@ -58,8 +67,37 @@ public class ClienteService {
 		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
 	}
 	
+	
+	public Cliente fromDTO(ClienteNewDTO objDTO) {
+		Cliente cli = new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getCpfOuCnpj(), TipoCliente.toEnum(objDTO.getTipo()));
+		Cidade cid = new Cidade(objDTO.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDTO.getLogradouro(), objDTO.getNumero(), objDTO.getComplemento(), objDTO.getBairro(), objDTO.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDTO.getTelefone1());
+		if(objDTO.getTelefone2() != null) {
+			cli.getTelefones().add(objDTO.getTelefone2());
+		}
+		if(objDTO.getTelefone3() != null) {
+			cli.getTelefones().add(objDTO.getTelefone3());
+		}
+		
+		
+		return cli;
+	}
+	
 	private void updateDate(Cliente newObj, Cliente obj) {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 	}
+	
+	@Transactional
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repo.save(obj);
+		endRepo.saveAll(obj.getEnderecos());
+		return obj;
+	}
+
+	
+	
 }
