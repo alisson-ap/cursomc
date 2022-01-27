f5aa9b193bf89b10c3782d40eab34cc23862fe91
Alisson Magalhaes <alisson08silva@gmail.com>
2022-01-27 16:37:16 -0300

    Validation cpf or cnpj

--- src/main/java/com/magalhaes/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/magalhaes/cursomc/dto/ClienteNewDTO.java
@@ -2,32 +2,53 @@ package com.magalhaes.cursomc.dto;
 
 import java.io.Serializable;
 
-public class ClienteNewDTO implements Serializable{
-	
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.magalhaes.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
+public class ClienteNewDTO implements Serializable {
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private Integer id;
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Email(message = "Email inválido")
 	private String email;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
+
 	private String cpfOuCnpj;
 	private Integer tipo;
-	
+
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String logradouro;
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String numero;
 	private String complemento;
 	private String bairro;
-	private String cep;
 	
+	@NotEmpty(message = "Preenchimento obrigatório")
+	private String cep;
+
+	@NotEmpty(message = "Preenchimento obrigatório")
 	private String telefone1;
 	private String telefone2;
 	private String telefone3;
-	
+
 	private Integer cidadeId;
-	
+
 	public ClienteNewDTO() {
-		
+
 	}
 
 	public Integer getId() {
@@ -141,7 +162,5 @@ public class ClienteNewDTO implements Serializable{
 	public void setCidadeId(Integer cidadeId) {
 		this.cidadeId = cidadeId;
 	}
-	
-	
 
 }
